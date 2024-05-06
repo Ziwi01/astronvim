16010e2d2110b380ae4816734f924bacdd79f6b5
Proveasio <Proveasio@hell.no>
2024-05-06 03:28:29 +0200

    feat: Add AstroCommunity plugins

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -23,12 +23,12 @@ return {
     "rmagatti/goto-preview", -- Edit preview in a floating windows. (`gpd`)
     config = function()
       require("goto-preview").setup {
-        width = 120,             -- Width of the floating window
-        height = 25,             -- Height of the floating window
+        width = 120, -- Width of the floating window
+        height = 25, -- Height of the floating window
         default_mappings = true, -- Bind default mappings
-        debug = false,           -- Print debug information
-        opacity = nil,           -- 0-100 opacity level of the floating window where 100 is fully transparent.
-        post_open_hook = nil,    -- A function taking two arguments, a buffer and a window to be ran as a hook.
+        debug = false, -- Print debug information
+        opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
         -- You can use "default_mappings = true" setup option
         -- Or explicitly set keybindings
         -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
@@ -52,7 +52,7 @@ return {
     end,
   },
   -- Remove trailing whitespace
-  { "ntpeters/vim-better-whitespace" },
+  -- { "ntpeters/vim-better-whitespace" },
   -- Running commands in TMUX split
   { "preservim/vimux" },
   -- Seamless TMUX panes Navigation
@@ -60,7 +60,7 @@ return {
   -- HELM support
   { "towolf/vim-helm" },
   -- Better quickfix window
-  { "kevinhwang91/nvim-bqf",         ft = "qf" },
+  { "kevinhwang91/nvim-bqf", ft = "qf" },
   -- Fallback search and replace (:Ack or <leader>a for search, :Acks or <leader>r to substitute)
   { "wincent/ferret" },
   -- Improve local search and replace
