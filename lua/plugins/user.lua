07bb93db28442a10d70fc734c91ebadb8526bfb4
Eryk K. <ziwi01@gmail.com>
2024-08-28 11:02:19 +0200

    feat: Add lua5.4 and luarocks plugins

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -85,6 +85,24 @@ return {
       }
     end,
   },
+  -- Additional Lua plugins
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
+  },
 
   -- customize alpha options
   -- {
