30366f006f586fce58e708e1736e68d2980fc15c
Eryk K. <ziwi01@gmail.com>
2024-08-28 11:02:05 +0200

    feat: Add Markview plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -6,6 +6,20 @@ return {
   { "will133/vim-dirdiff" },
   -- Markdown support
   { "SidOfc/mkdx" },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
   -- Fuzzy finder
   { "junegunn/fzf" },
   { "junegunn/fzf.vim" },
